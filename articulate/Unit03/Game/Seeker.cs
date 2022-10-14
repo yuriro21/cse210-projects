1e3946584019854bd1731ecdbbbb02ee9b3ed80c
yuriro21 <edisson.ruiz21@gmail.com>
2022-10-13 22:16:08 -0600

    articulate done

--- articulate/Unit03/Game/Seeker.cs
+++ articulate/Unit03/Game/Seeker.cs
@@ -13,14 +13,20 @@ namespace Unit03.Game
         /// The responsibility of a Seeker is to keep track of its location.
         /// </para>
         /// </summary>
-
+    public class Seeker
+    {
+        private int location = 0;
 
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
-       
+       public Seeker () 
+       {
+        Random random = new Random();
+        location = random.Next(1001);
+       }
 
     // 3) Create the GetLocation() method. Use the following method comment.
         
@@ -28,7 +34,10 @@ namespace Unit03.Game
         /// Gets the current location.
         /// </summary>
         /// <returns>The current location as an int.</returns>
-        
+        public int GetLocation() {
+            return location;
+
+        }
 
     // 4) Create the MoveLocation(int location) method. Use the following method comment.
         
@@ -36,5 +45,11 @@ namespace Unit03.Game
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
+        public void MoveLocation (int location){
+            
+            this.location = location;
+        }
+
+    }
     
 }
